f57331f0746951245ea6caa3b7ca6d4e998829eb
mksantos98 <mikael.ffc@gmail.com>
2021-01-25 20:01:40 -0300

    Adicionando o Confirma

--- sistema2/banco_de_dados/confirma.php
+++ sistema2/banco_de_dados/confirma.php
@@ -1,9 +1,8 @@
 <?php
-
-    include_once 'banco_de_dados/conexao.php';  
+include_once '../banco_de_dados/conexao.php';
 $id = filter_input(INPUT_GET, 'id', FILTER_SANITIZE_SPECIAL_CHARS);
+echo "<script>alert('Deseja Mesmo EXCLUIR esse Funcionário?');</script>";
 echo 'Deseja mesmo EXCLUIR esse Funcionário? ';
 echo "<a href='delete.php?id=$id'>Sim</a> | ";
 echo "<a href='../consultas.php?id=$id'>Não</a>";
-
 ?>
